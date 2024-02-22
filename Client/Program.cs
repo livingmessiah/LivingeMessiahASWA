7fdf13a70b38136e6b500a8fd3f6f5646f9d141a
John Marsing <JohnMarsing@outlook.com>
2024-02-22 08:48:34 -0700

    Started to add Blazored.Toast

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Client;
 using Blazored.LocalStorage;
+using Blazored.Toast;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazoredToast();
 builder.Services.AddScoped(sp => new HttpClient
 {
 	BaseAddress = new Uri(builder.Configuration["API_Prefix"] 
